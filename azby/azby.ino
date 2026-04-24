b381d84e214d5f1f88614a66028f1dd45f3ad798
Xaer-xx <xaer.xddd@gmail.com>
2026-04-24 19:49:27 +0200

    Refactor azby.ino for improved sensor handling

--- azby/azby.ino
+++ azby/azby.ino
@@ -1,251 +1,199 @@
-#include <Wire.h>
 #include <QTRSensors.h>
+#include <Wire.h>
 #include <VL53L1X.h>
 
 QTRSensors qtr;
+VL53L1X tof;
+
 const uint8_t SensorCount = 8;
 uint16_t sensorValues[SensorCount] = { 0 };
 
-VL53L1X sensor;
-uint16_t lastDistance = 0;
+int stav = 1;               // 0=wait calib, 1=calibrate, 2=line follow, 3=stop
+bool calibration_done = false;
+long casstartu = 0;
 
+// Line follower
+int baseSpeed = 240;
+float kp = 8.3;
+int rych_tocenia = 255;
+int last_position = 3500;
+
+// VL53L1X
+#define TOF_OBSTACLE_MM  200   // Vzdialenosť v mm – pri menšej robot zastaví
+bool tof_ok = false;
+
+//--------------------------------------------------------------------
+// Pins
+//--------------------------------------------------------------------
+
+// MX1508
 #define LEFT_FWD  6
 #define LEFT_BWD  5
 #define RIGHT_FWD 10
 #define RIGHT_BWD 9
 
-int stav = 1;
-bool calibration_done = false;
-bool dekaRezim = false;
-long casstartu = 0;
-int last_position = 3500;
-int last_error = 0;
+//--------------------------------------------------------------------
+// MOTORS (forward + backward)
+//--------------------------------------------------------------------
 
-// ======================================================
-// ==================== DOLADENIE =======================
-// ======================================================
-int IgnoreTime         = 2;
-int atencion           = 15;
-
-int baseSpeed          = 240; 
-float kp               = 0.1; 
-float kd               = 0.7; 
-
-int rych_tocenia       = 255;
-int tehla              = 120;
-
-int sharpTurnThreshold = 200;
-int minTurnSpeed       = 40;
-
-int T_TURN_90   = 280;
-int T_OBIST_BOK = 400;
-int T_OBIST_DLZ = 600;
-
-// --- NOVÉ: Čas prejazdu cez farbu (P7) a prerušenú čiaru (P2) ---
-int limitSlepejJazdy  = 800; 
-
-// ======================================================
-// ============= MERANIE VZDIALENOSTI ===================
-// ======================================================
-long zmerajVzdialenost() {
-  if (sensor.dataReady()) {
-    lastDistance = sensor.read(false);
-  }
-  return lastDistance / 10;
-}
-
-// ======================================================
-// ==================== MOTORY ==========================
-// ======================================================
 void setMotors(int left, int right) {
-  left  = constrain(left,  -255, 255);
-  right = constrain(right, -255, 255);
-
-  analogWrite(LEFT_FWD,  left  > 0 ? left  : 0);
-  analogWrite(LEFT_BWD,  left  < 0 ? -left : 0);
-  analogWrite(RIGHT_FWD, right > 0 ? right : 0);
-  analogWrite(RIGHT_BWD, right < 0 ? -right : 0);
-}
-
-void zastavsa()               { setMotors(0, 0); }
-void tocVpravo(int a, int ms) { setMotors(a, -a); delay(ms); zastavsa(); }
-void tocVlavo(int a, int ms)  { setMotors(-a, a); delay(ms); zastavsa(); }
-void idDopredu(int r, int ms) { setMotors(r, r); delay(ms); zastavsa(); }
-
-// ======================================================
-// ======= ADAPTIVNÁ RÝCHLOSŤ ===========================
-// ======================================================
-int getAdaptiveSpeed(int error) {
-  int absError = abs(error);
-  if (absError <= sharpTurnThreshold) return baseSpeed;
-  return constrain(
-    map(absError, sharpTurnThreshold, 3500, baseSpeed, minTurnSpeed),
-    minTurnSpeed, baseSpeed
-  );
-}
-
-// ======================================================
-// ========= PD REGULÁTOR - JADRO ROBOTA ================
-// ======================================================
-void pidStep() {
-  uint16_t position = qtr.readLineBlack(sensorValues);
-  int error = (int)position - 3500;
-
-  // -------- STRATA ČIARY --------
-  if (position == 0 || position == 7000) {
-
-    if (last_position < 1000 && position == 0) {
-      // Ľavá strana - tocíme doľava kým nenájdeme čiaru
-      do {
-        setMotors(-rych_tocenia, rych_tocenia);
-        position = qtr.readLineBlack(sensorValues);
-      } while (position == 0);
-
-    } else if (last_position > 6000 && position == 7000) {
-      // Pravá strana - točíme doprava
-      do {
-        setMotors(rych_tocenia, -rych_tocenia);
-        position = qtr.readLineBlack(sensorValues);
-      } while (position == 7000);
-
-    } else {
-      // Stred - PRERUŠENÁ ČIARA (P2), FARBA (P7) alebo návrat z tehly
-      unsigned long t = millis();
-      while ((position == 0 || position == 7000) && millis() - t < limitSlepejJazdy) {
-        setMotors(baseSpeed, baseSpeed);
-        position = qtr.readLineBlack(sensorValues);
-      }
-    }
-
-    last_position = position;
-    last_error = 0;   
-    return;
-  }
-
-  // -------- PD VÝPOČET --------
-  int correction = (int)(kp * error) + (int)(kd * (error - last_error));
-  int speed = getAdaptiveSpeed(error);
-
-  setMotors(speed - correction, speed + correction);
-
-  last_position = position;
-  last_error = error;   
-}
-
-// ======================================================
-// ============= VYHYBANIE SA TEHLE =====================
-// ======================================================
-void vyhybajSaTehle() {
-  Serial.println("TEHLA - obchadzanie");
-  zastavsa(); delay(100);
-
-  tocVpravo(tehla, T_TURN_90); delay(100);
-  idDopredu(baseSpeed, T_OBIST_BOK); delay(100);
-  tocVlavo(tehla, T_TURN_90); delay(90);
-  idDopredu(baseSpeed, T_OBIST_DLZ); delay(100);
-  tocVlavo(tehla, T_TURN_90); delay(80);
-
-  unsigned long timeout = millis();
-  while (millis() - timeout < 3000) {
-    setMotors(baseSpeed, baseSpeed);
-    uint16_t pos = qtr.readLineBlack(sensorValues);
-    if (pos > 500 && pos < 6500) { zastavsa(); break; }
-  }
-  delay(100);
-
-  tocVpravo(tehla, T_TURN_90); delay(100);
-  last_position = 3500;
-  last_error = 0;
-}
-
-// ======================================================
-// ========= DETEKCIA TEHLA vs DEKA =====================
-// ======================================================
-void detekcia(int a) {
-  zastavsa(); delay(100);
-  tocVpravo(a, 280); delay(50);
-
-  long d = zmerajVzdialenost();
-
-  if (d < 60 && d > 0) {
-    Serial.println("DEKA");
-    tocVlavo(a, 280); delay(100);
-    dekaRezim = true;
+  if (left >= 0) {
+    analogWrite(LEFT_FWD, constrain(left, 0, 255));
+    analogWrite(LEFT_BWD, 0);
   } else {
-    Serial.println("TEHLA");
-    tocVlavo(a, 280); delay(100);
-    vyhybajSaTehle();
+    analogWrite(LEFT_FWD, 0);
+    analogWrite(LEFT_BWD, constrain(-left, 0, 255));
+  }
+
+  if (right >= 0) {
+    analogWrite(RIGHT_FWD, constrain(right, 0, 255));
+    analogWrite(RIGHT_BWD, 0);
+  } else {
+    analogWrite(RIGHT_FWD, 0);
+    analogWrite(RIGHT_BWD, constrain(-right, 0, 255));
   }
 }
 
-// ======================================================
-// ====================== SETUP =========================
-// ======================================================
+void zastavsa() {
+  analogWrite(LEFT_FWD, 0);
+  analogWrite(LEFT_BWD, 0);
+  analogWrite(RIGHT_FWD, 0);
+  analogWrite(RIGHT_BWD, 0);
+}
+
+//--------------------------------------------------------------------
+// VL53L1X – čítanie vzdialenosti (neblokujúce)
+//--------------------------------------------------------------------
+
+bool isObstacleDetected() {
+  if (!tof_ok) return false;
+  if (tof.dataReady()) {
+    uint16_t dist = tof.read();
+    Serial.print("TOF dist: ");
+    Serial.println(dist);
+    if (dist > 0 && dist < TOF_OBSTACLE_MM) {
+      return true;
+    }
+  }
+  return false;
+}
+
+//--------------------------------------------------------------------
+// Setup
+//--------------------------------------------------------------------
+
 void setup() {
   Serial.begin(115200);
+  Serial.println("Start MCU");
+
+  // VL53L1X init
   Wire.begin();
   Wire.setClock(400000);
-
-  sensor.setTimeout(500);
-  if (!sensor.init()) {
-    Serial.println("VL53L1X nenajdeny!");
-    while (1);
+  tof.setTimeout(500);
+  if (tof.init()) {
+    tof.setDistanceMode(VL53L1X::Long);
+    tof.setMeasurementTimingBudget(20000); // 20 ms – rýchle meranie
+    tof.startContinuous(20);
+    tof_ok = true;
+    Serial.println("VL53L1X OK");
+  } else {
+    Serial.println("VL53L1X NENAJDENY – pokracujem bez TOF");
   }
-  sensor.setDistanceMode(VL53L1X::Long);
-  sensor.setMeasurementTimingBudget(20000);
-  sensor.startContinuous(10);
 
   qtr.setTypeRC();
   qtr.setSensorPins((const uint8_t[]){ 15, 16, 17, 2, 3, 4, 7, 8 }, SensorCount);
   qtr.setEmitterPin(12);
 
-  pinMode(LEFT_FWD, OUTPUT); pinMode(LEFT_BWD, OUTPUT);
-  pinMode(RIGHT_FWD, OUTPUT); pinMode(RIGHT_BWD, OUTPUT);
+  pinMode(LEFT_FWD, OUTPUT);
+  pinMode(LEFT_BWD, OUTPUT);
+  pinMode(RIGHT_FWD, OUTPUT);
+  pinMode(RIGHT_BWD, OUTPUT);
   pinMode(LED_BUILTIN, OUTPUT);
 
-  zastavsa();
+  digitalWrite(LEFT_FWD, LOW);
+  digitalWrite(LEFT_BWD, LOW);
+  digitalWrite(RIGHT_FWD, LOW);
+  digitalWrite(RIGHT_BWD, LOW);
+
   delay(1000);
+  Serial.println("Configuration done");
+
   stav = 1;
+  Serial.println("Auto-calibration in 3 sec - prepare to move robot!");
   delay(3000);
 }
 
-// ======================================================
-// ====================== LOOP ==========================
-// ======================================================
+//--------------------------------------------------------------------
+// Main loop
+//--------------------------------------------------------------------
+
 void loop() {
 
-  if (dekaRezim) {
-    unsigned long zaciatok = millis();
-    while (millis() - zaciatok < (IgnoreTime * 1000)) {
-      pidStep();   
-    }
-    dekaRezim = false;
+  // Kontrola prekazky vo vsetkych jazdnych stavoch
+  if (stav == 2 && isObstacleDetected()) {
+    Serial.println("PREKAZKA! Zastavujem.");
+    stav = 3;
+  }
+
+  if (stav == 0) {
+    stav = 1;
+    Serial.println("Prechadzam na kalibraciu");
 
   } else if (stav == 1) {
-    Serial.println("Kalibracia...");
     digitalWrite(LED_BUILTIN, HIGH);
-    for (uint16_t i = 0; i < 400; i++) {
-      setMotors((i / 20) % 2 == 0 ? 50 : -50,
-                (i / 20) % 2 == 0 ? -50 : 50);
+    Serial.println("Kmitava kalibracia (zmena kazdych 50 cyklov)...");
+
+    for (uint16_t i = 0; i < 500; i++) {
+      if ((i / 10) % 2 == 0) {
+        setMotors(50, -50);
+      } else {
+        setMotors(-50, 50);
+      }
       qtr.calibrate();
-      delay(5);
+      delay(4);
     }
+
     zastavsa();
     digitalWrite(LED_BUILTIN, LOW);
+
+    Serial.print("Kalibracia hotova. Min hodnoty: ");
+    for (uint8_t i = 0; i < SensorCount; i++) {
+      Serial.print(qtr.calibrationOn.minimum[i]);
+      Serial.print(' ');
+    }
+    Serial.println();
+
     calibration_done = true;
     stav = 2;
-    last_error = 0;
-    Serial.println("Kalibracia hotova.");
+
+    Serial.println("Priprava na start...");
     delay(2000);
     casstartu = millis();
 
   } else if (stav == 2) {
-    long vzd = zmerajVzdialenost();
-    if (vzd > 0 && vzd < atencion) {
-      detekcia(tehla);
-      return;
+    uint16_t position = qtr.readLineBlack(sensorValues);
+
+    if (last_position < 1000 && position == 0) {
+      setMotors(rych_tocenia, 0);
+    } else if (last_position > 6000 && position == 7000) {
+      setMotors(0, rych_tocenia);
+    } else if ((last_position < 6000 && last_position > 1000) && (position == 0 || position == 7000)) {
+      last_position = position;
+      while ((position == 0 || position == 7000) && (last_position == 0 || last_position == 7000)) {
+        setMotors(baseSpeed, baseSpeed);
+        position = qtr.readLineBlack(sensorValues);
+        last_position = position;
+      }
+    } else {
+      int error = (int)position - 3500;
+      int correction = error * kp / 100;
+
+      int leftSpeed  = baseSpeed - correction;
+      int rightSpeed = baseSpeed + correction;
+
+      setMotors(leftSpeed, rightSpeed);
+      last_position = position;
     }
-    pidStep();
 
   } else if (stav == 3) {
     zastavsa();
